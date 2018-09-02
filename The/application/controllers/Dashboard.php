8deb92292abe82ea4c022808653028e04f2c948b
chelseabowo <41238570+chelseabowo@users.noreply.github.com>
2018-09-02 18:41:46 +0700

    Update Controller Dasboard Utama

--- The/application/controllers/Dashboard.php
+++ The/application/controllers/Dashboard.php
@@ -129,17 +129,14 @@ class Dashboard extends CI_Controller {
 					redirect(base_url('Dashboard/index'));	
 				}
 			}else if($role['m_role_id']=='9'){ //wali_murid
-				if($role['d_sekolah_id']=='0' && $role['d_kelas_id']=='0'){
+				if($family['m_user_id_son'] =='0'){
 					$this->session->set_userdata($data_session);
 					redirect(base_url('9/C_starter/index'));
-				}else if($role['d_sekolah_id']!='0' && $role['d_kelas_id']!='0' && $role['is_verified']=='1'){
+				}else if($family['m_user_id_son'] !='0'){
 					$this->session->set_userdata($data_session);
 					redirect(base_url('9/C_dashboard/index'));
-				}else if($role['is_verified']=='0'){
-					echo "<script>alert('UserID Anda Belum di Verifikasi,Mohon Hubungi Admin Sekolah')</script>";
-					echo "<script>window.location=history.go(-1)</script>";
 				}else{
-					redirect(base_url('Dashboard/index'));	
+					redirect(base_url('Dashboard/index'));
 				}
 			}else{
 				redirect(base_url('Dashboard/index'));
